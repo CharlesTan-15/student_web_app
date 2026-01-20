23495ecdb6397809e483d812df8f87efba2d0e7e
MiguelWalter <miguelwalteruy18@gmail.com>
2026-01-20 12:34:53 +0800

    updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function validateFrom() {
+    let name = document.getElementById("name").value;
+    let grade = document.getElementById("grade").value;
+
+    if (name === "" || grade === "") {
+        alert("Please fill in all fields.")
+        return false;
+    }
+
+    if (grade < 0 || grade > 100) {
+        alert("grade must be between 0 and 100");
+        return false;
+    }
+
+    return true;
+}
 
